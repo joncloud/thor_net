54815eef7774e94a0314cd5bf971d52b3465030e
joncloud <jdberube@gmail.com>
2015-12-14 17:24:38 -0800

    Moved Dice into its own file. Minor tweak to method.

--- src/ThorNet.Terminal/Program.cs
+++ src/ThorNet.Terminal/Program.cs
@@ -8,6 +8,11 @@ using System.Threading.Tasks;
 namespace ThorNet.Terminal
 {
     public class Program : Thor {
+
+        protected override string GetPackageName() {
+            return "dice";
+        }
+
         public static void Main(string[] args) {
             Start<Program>(args);
         }
@@ -18,73 +23,26 @@ namespace ThorNet.Terminal
             if (Dice.TryParse(expr, out dice)) {
                 Console.WriteLine($"Rolling {expr}");
                 int[] results = dice.Roll();
+                int total = 0;
                 foreach (int result in results) {
                     Console.WriteLine($"\t{result}");
+                    total += result;
                 }
+
+                string text = total.ToString();
+                Console.Write("\t");
+                Console.WriteLine(new string('-', text.Length));
+                Console.Write("\t");
+                Console.WriteLine(text);
             } 
             else {
                 Console.WriteLine("Invalid dice expression.");
             }
         }
         
-        [Desc("roll EXPR", "rolls dice using D&D notation, i.e., 2d6 asynchronously.")]
+        [Desc("rollAsync EXPR", "rolls dice using D&D notation, i.e., 2d6 asynchronously.")]
         public Task rollAsync(string expr) {
             return Task.Run(() => roll(expr));
-        }
-    }
-    
-    public class Dice {
-        
-        private Random _random = new Random();
-        
-        public Dice(int sides)
-            : this(sides, 1) {
-            
-        }
-        
-        public Dice(int sides, int count) {
-            if (sides <= 0) throw new ArgumentOutOfRangeException(nameof(sides));
-            if (count <= 0) throw new ArgumentOutOfRangeException(nameof(count));
-            
-            Count = count;
-            Sides = sides;
-        }
-        
-        public int Count { get; }
-        public int Sides { get; }
-        
-        public int[] Roll() {
-            int[] results = new int[Count];
-            for (int i = 0; i < results.Length; i++) {
-                results[i] = _random.Next(1, Sides + 1);
-            }
-            return results;
-        }
-        
-        public static bool TryParse(string text, out Dice dice) {
-            
-            dice = null;
-            
-            string[] parts = text.Split(new [] { 'd' }, StringSplitOptions.RemoveEmptyEntries);
-            int[] values = new int[parts.Length];
-            
-            for (int i = 0; i < parts.Length; i++) {
-                if (!int.TryParse(parts[i], out values[i])) {
-                    return false;
-                }
-            }
-            
-            switch (parts.Length) {
-                case 1:
-                    dice = new Dice(values[0]);
-                    return true;
-                    break;
-                case 2:
-                    dice = new Dice(values[1], values[0]);
-                    return true;
-                default:
-                    return false;
-            }
         }
     }
 }
