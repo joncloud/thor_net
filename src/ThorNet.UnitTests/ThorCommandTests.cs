5b90c0f8a4e34714cb3dfbf6c4e68710f3715202
joncloud <jdberube@gmail.com>
2016-05-21 07:59:15 -0700

    Added better messaging for missing parameters.

---
+++ src/ThorNet.UnitTests/ThorCommandTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ThorNet.UnitTests
+{
+    public class ThorCommandTests
+    {
+        [InlineData("a")]
+        [InlineData("a,b")]
+        [Theory]
+        public void BindArguments_ThrowsMissingParameterException(string argsList)
+        {
+            var args = new List<string>(argsList.Split(','));
+            var target = new Target();
+
+            var command = target.Commands[nameof(Target.RequiredParameter)];
+
+            Assert.Throws<MissingParameterException>(() =>
+            {
+                command.BindArguments(args);
+            });
+        }
+
+        public class Target : Thor
+        {
+            public void RequiredParameter(string a, string b, string c) { }
+        }
+    }
+}
